21f70e6e190d730f66af39758303a81d92f16d2f
jyiwm <joshua.yi@warnermedia.com>
2022-04-27 15:15:21 -0400

    Add Apollo Client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query GetBooks {
+        books {
+          author
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp
