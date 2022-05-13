4d241fc6d6d3440a33e5a01ee2e61da8f71c4cfc
jyiwm <joshua.yi@warnermedia.com>
2022-05-12 22:19:59 -0400

    Add twitter api

--- components/board/board.tsx
+++ components/board/board.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import { BoardContentProps, BoardContent } from './boardContent';
 
 type BoardProps = {
@@ -6,8 +8,13 @@ type BoardProps = {
 };
 
 export const Board = ({ id, content }: BoardProps) => {
+    const initializeTwitterApi = () => {
+    };
     return (
         <div>
+            <Script src="https://platform.twitter.com/widgets.js" strategy="lazyOnload"
+                    onLoad={initializeTwitterApi}
+             />
             <div>{id}</div>
             <ul>
                 {content.map((content, index) => {
