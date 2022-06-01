8c41e22f08696b07f2a4fb6899519b47eb4ad00c
jyiwm <joshua.yi@warnermedia.com>
2022-06-01 19:12:16 -0400

    Add rows to list

--- pages/feeds/create/list.tsx
+++ pages/feeds/create/list.tsx
@@ -1,10 +1,11 @@
 import dynamic from 'next/dynamic';
-import { useEffect, useRef } from 'react';
+import { useState, } from 'react';
 
 const containerSelector = 'tbody';
 
 // We must use a dynamic import here because the draggable does not work with SSR
 // It must only be available client side, due to its usage of the window object.
+// @ts-ignore
 const SortableDynamic = dynamic(() => import('@shopify/draggable/lib/sortable').then(
     (mod: any) => {
         const Sortable = mod.default;
@@ -14,11 +15,40 @@ const SortableDynamic = dynamic(() => import('@shopify/draggable/lib/sortable').
         }
         const sortable = new Sortable(containers, {
             draggable: 'tr',
+            handle: '.checkbox',
         });
     }
 ), { ssr: false });
 
-const CreateList = () => {
+type CreateListProps = {
+    data?: any[];
+}
+
+interface ListRow {
+    isChecked: boolean,
+    url: string,
+}
+
+const CreateList = ({data} : CreateListProps) => {
+    const mockRows: ListRow[] = [
+        {isChecked: true, url: 'https://www.google.com'},
+        {isChecked: false, url: 'https://www.yahoo.com'},
+        {isChecked: true, url: 'https://www.facebook.com'},
+    ]
+    data = mockRows;
+    const [rows, setRows] = useState(data ?? []);
+
+    const handleOnCheck = (event: any) => {
+
+    };
+
+    const handleOnInputChange = ({row, index, event}: any) => {
+        const oldRows = rows;
+        const oldRow = oldRows[index];
+        const newRow = {...oldRow, [event.target.name]: event.target.value};
+        oldRows[index] = newRow;
+        setRows(oldRows);
+    };
     return (
         <>
             <SortableDynamic></SortableDynamic>
@@ -32,141 +62,36 @@ const CreateList = () => {
                                     <input type="checkbox" className="checkbox" />
                                 </label>
                             </th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>URL</th>
                             <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        {/* row 1 */}
-                        <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <td>
-                                <div className="flex items-center space-x-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle w-12 h-12">
-                                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-                                        </div>
+                        {rows.map((row, index) => (
+                            <tr key={index}>
+                                <th>
+                                    <label>
+                                        <input name="isChecked" type="checkbox" className="checkbox" checked={row.isChecked} onChange={handleOnCheck}/>
+                                    </label>
+                                </th>
+                                <td>
+                                    <div className="flex">
+                                        {/* <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div> */}
+                                        <input name="url" type="text" className="input input-bordered w-full max-w-xs" value={row.url} onChange={(event) => handleOnInputChange({row, index, event})}/>
                                     </div>
-                                    <div>
-                                        <div className="font-bold">Hart Hagerty</div>
-                                        <div className="text-sm opacity-50">United States</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Zemlak, Daniel and Leannon
-                                <br />
-                                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                            </td>
-                            <td>Purple</td>
-                            <th>
-                                <button className="btn btn-ghost btn-xs">details</button>
-                            </th>
-                        </tr>
-                        {/* row 2 */}
-                        <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <td>
-                                <div className="flex items-center space-x-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle w-12 h-12">
-                                            <img src="/tailwind-css-component-profile-3@56w.png" alt="Avatar Tailwind CSS Component" />
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div className="font-bold">Brice Swyre</div>
-                                        <div className="text-sm opacity-50">China</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Carroll Group
-                                <br />
-                                <span className="badge badge-ghost badge-sm">Tax Accountant</span>
-                            </td>
-                            <td>Red</td>
-                            <th>
-                                <button className="btn btn-ghost btn-xs">details</button>
-                            </th>
-                        </tr>
-                        {/* row 3 */}
-                        <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <td>
-                                <div className="flex items-center space-x-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle w-12 h-12">
-                                            <img src="/tailwind-css-component-profile-4@56w.png" alt="Avatar Tailwind CSS Component" />
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div className="font-bold">Marjy Ferencz</div>
-                                        <div className="text-sm opacity-50">Russia</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Rowe-Schoen
-                                <br />
-                                <span className="badge badge-ghost badge-sm">Office Assistant I</span>
-                            </td>
-                            <td>Crimson</td>
-                            <th>
-                                <button className="btn btn-ghost btn-xs">details</button>
-                            </th>
-                        </tr>
-                        {/* row 4 */}
-                        <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <td>
-                                <div className="flex items-center space-x-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle w-12 h-12">
-                                            <img src="/tailwind-css-component-profile-5@56w.png" alt="Avatar Tailwind CSS Component" />
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div className="font-bold">Yancy Tear</div>
-                                        <div className="text-sm opacity-50">Brazil</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Wyman-Ledner
-                                <br />
-                                <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
-                            </td>
-                            <td>Indigo</td>
-                            <th>
-                                <button className="btn btn-ghost btn-xs">details</button>
-                            </th>
-                        </tr>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                     {/* foot */}
                     <tfoot>
                         <tr>
                             <th></th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>URL</th>
                             <th></th>
                         </tr>
                     </tfoot>
